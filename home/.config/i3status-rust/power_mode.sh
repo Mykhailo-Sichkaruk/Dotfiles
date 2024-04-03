9322b677ae9fef5648c56c73e8110a74396666e6
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2024-04-03 03:54:32 +0200

    Improve i3-status-rust power.sh

--- home/.config/i3status-rust/power_mode.sh
+++ home/.config/i3status-rust/power_mode.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
+# This script reads the power mode and returns json to be used in i3status-rust
 # Read the value from /sys/firmware/acpi/platform_profile
-profile=$(cat /sys/firmware/acpi/platform_profile)
+if [[ -f /sys/firmware/acpi/platform_profile ]]; then
+    profile=$(cat /sys/firmware/acpi/platform_profile)
+fi
+
+
+# If profil is empty, read from /sys/firmware/acpi/pm_profile
+if [[ -z "$profile" ]]; then
+    profile=$(cat /sys/firmware/acpi/pm_profile)
+fi
 
 # Define colors and corresponding symbols
 GREY="{\"icon\":\"\",\"state\":\"Info\", \"text\": \"\\Uf111\"}" # quiet 
 ORANGE="{\"icon\":\"\",\"state\":\"Warning\", \"text\": \"\Uf111\"}" # balanced
 RED="{\"icon\":\"\",\"state\":\"Critical\", \"text\": \"\Uf111\"}" # performance
-json = true
+json=true
 
 # Check the value and print the corresponding symbol
 if [[ "$profile" == "performance" ]]; then
@@ -15,5 +25,5 @@ elif [[ "$profile" == "balanced" ]]; then
 elif [[ "$profile" == "quiet" ]]; then
     echo -e "$GREY"
 else
-    echo "Unknown profile"
+    echo "Unknown profile: $profile"
 fi
