5a67a9a62a86b82195751fd835cfcdb25d7a0348
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2024-10-09 19:10:45 +0200

    Update ni

--- home/.config/home-manager/home.nix
+++ home/.config/home-manager/home.nix
@@ -1,110 +1,65 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 let
-
-  nixpkgs = import <nixpkgs> {
-    config.allowUnfree = true;
+  myGlibcLocales = pkgs.glibcLocales.override {
+    locales = [ "en_US.UTF-8" ];
   };
+  # Import shell configuration
+  shellConfig = import ./shell.nix {};
 
-  shellConfig = import ~/.config/home-manager/shell.nix {};
+  # Import GUI configuration
+  youtubeMusicConfig = import ./programs/gui.nix { inherit pkgs; };
 in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "ms";
   home.homeDirectory = "/home/ms";
+  home.stateVersion = "24.05";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  # Define the packages to be installed
   home.packages = shellConfig ++ [
-    pkgs.alacritty         # Terminal emulator
-    pkgs.mysql-workbench   # MySQL database design and management tool
-    pkgs.i3                # Tiling window manager
-    pkgs.i3status-rust     # i3 status bar
-    pkgs.youtube-music     # Music streaming service
-    pkgs.vscode            # Code editor
-    pkgs.obs-studio        # Open Broadcaster Software
-    pkgs.discord           # Chat and collaboration platform
-    pkgs.pipewire          # Audio and video server
-    pkgs.slack             # Collaboration platform for teams
-    pkgs.telegram-desktop  # Telegram client
-    pkgs.dbeaver-bin           # Database manager
-    pkgs.drawio            # Diagramming tool
-    pkgs.mpv               # Video player
-    pkgs.neomutt           # Email client
-    pkgs.gtkmm4            # C++ bindings for GTK+4
-    pkgs.sqlitebrowser     # SQLite database management tool
-    pkgs.wayland           # Display server protocol
-    pkgs.ffmpeg            # Multimedia framework
-    pkgs.obsidian          # Note-taking software
-    pkgs.pdftk             # PDF toolkit
-    pkgs.vimiv-qt          # Image viewer and Vim-like interface
-    pkgs.neomutt           # Email client
-    pkgs.ripgrep           # CLI search tool
-    pkgs.wayland           # Display server protocol
-    pkgs.gtk4              # Toolkit for creating GUIs
-    pkgs.imagemagick       # Image processing tools
-    pkgs.zathura           # PDF / DJVU file viewer
-    pkgs.vlc               # Media player
-    pkgs.gnome-keyring     # GNOME's password management tool
-    pkgs.calc              # Calculator application
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    # pkgs.xorg.xserver  # Xorg server from Nix
+    # pkgs.xorg.xinit    # If you use `startx` or `xinit` to start i3
+    # pkgs.sxhkd           # Simple X hotkey daemon
+    pkgs.pulsemixer    # PulseAudio mixer
+    pkgs.easyeffects
+    # pkgs.vifm
+    pkgs.mysql-workbench
+    # pkgs.i3
+    # pkgs.i3status-rust
+    pkgs.youtube-music  # Install YouTube Music here
+    # pkgs.vscode
+    pkgs.obs-studio
+    pkgs.discord
+    pkgs.betterdiscordctl
+    # pkgs.pipewire
+    pkgs.slack
+    pkgs.telegram-desktop
+    pkgs.dbeaver-bin
+    pkgs.drawio
+    pkgs.mpv
+    pkgs.obsidian
+    pkgs.vimiv-qt
+    pkgs.zathura
+    pkgs.calc
+    pkgs.vieb
+    pkgs.google-chrome
+    pkgs.peek
+    # pkgs.kooha
+    pkgs.mpv               # Media player
+    # pkgs.glibcLocales
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ms/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    XDG_DATA_DIRS = "${config.home.homeDirectory}/.nix-profile/share:/usr/share:/usr/local/share";
+    LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+    LC_ALL = "en_US.UTF-8";
+    LANG = "en_US.UTF-8";
   };
+  # Add the external GUI config (which includes the YouTube Music desktop entry)
+  imports = [
+    ./programs/gui.nix
+  ];
 
-  # Let Home Manager install and manage itself.
+  # Let Home Manager manage itself
   programs.home-manager.enable = true;
 }
---
+++ home/.config/home-manager/programs/gui.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  # Define a desktop entry for YouTube Music
+  xdg.desktopEntries.youtube-music = {
+    name = "Youtube Music";
+    genericName = "Music streaming service";
+    exec = "youtube-music"; # Make sure this is the correct command
+    icon = "youtube-music"; # Optionally set an icon
+    comment = "Music streaming service";
+    categories = [ "AudioVideo" "Music" ];
+  };
+}
